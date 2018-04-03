e8e38d96fcf168db1934189281337c81bea26216
Sammy Akharaz <ketrixz@gmail.com>
2018-04-03 18:49:29 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,27 @@
+function add (a,b) {
+  return a+b
+}
+function subtract (a,b) {
+  return a-b
+}
+function multiply (a,b) {
+  return a*b
+}
+function divide(a,b) {
+  return a/b
+}
+function inc (n) {
+  
+}
+function dec(n) {
+  
+}
+function (n) {
+  
+}
+function (n) {
+  
+}
+function (n) {
+  
+}
